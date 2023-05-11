f7ee0429ba9ac02b83bba907bfbf2bb6c8329896
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-05-10 22:51:57 -0700

    Add 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,9 +5,9 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end='')
         elif i % 5 == 0:
-            print("Buzz", end=' ')
+            print("Buzz", end='')
         elif i % 3 == 0:
-            print("Fizz", end=' ')
+            print("Fizz", end='')
         else:
             print(i, end='')
 
