e7bce66de11070edcdffb1fabd75937f7e9f7305
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-06-14 06:31:46 -0700

    Add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script defines a function that reads a text file (UTF8)
+and prints its contents to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Function: read_file()
+    Prints the content of a file
+    """
+    with open(filename) as file_object:
+        read_data = file_object.read()
+    print(read_data)
