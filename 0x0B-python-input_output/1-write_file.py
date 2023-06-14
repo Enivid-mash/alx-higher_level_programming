9b13d5d6daae55e6cc90acf4211c9354017ec9c7
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-06-14 07:15:40 -0700

    Add 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
-This script defines a function that reads a text file (UTF8)
-and prints its contents to stdout.
+This script defines a function that writes text to a file (UTF8).
 """
 
 
 def write_file(filename="", text=""):
     """
-    Function: read_file()
-    Prints the content of a file
+    Function: write_file()
+    Writes the given text to the specified file.
     """
     with open(filename, 'w') as file_object:
         return file_object.write(text)
-
