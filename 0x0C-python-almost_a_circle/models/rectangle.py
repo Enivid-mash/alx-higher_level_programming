f45458f445178a4aa4f1e7611f9fb8fe7dd3d1b7
Mawunyo on axl sandbox-turtle python <dmashigbi@gmail.com>
2023-06-19 18:51:46 -0700

    Update models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,80 +32,52 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-        Getter for the width attribute.
-
-        Returns:
-            int: The width of the rectangle.
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Setter for the width attribute.
-
-        Args:
-            value (int): The width of the rectangle.
-        """
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        """
-        Getter for the height attribute.
-
-        Returns:
-            int: The height of the rectangle.
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Setter for the height attribute.
-
-        Args:
-            value (int): The height of the rectangle.
-        """
-        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
-        """
-        Getter for the x-coordinate attribute.
-
-        Returns:
-            int: The x-coordinate of the rectangle's position.
-        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
-        Setter for the x-coordinate attribute.
-
-        Args:
-            value (int): The x-coordinate of the rectangle's position.
-        """
-        self.__x = value
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
-        """
-        Getter for the y-coordinate attribute.
-
-        Returns:
-            int: The y-coordinate of the rectangle's position.
-        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        Setter for the y-coordinate attribute.
-
-        Args:
-            value (int): The y-coordinate of the rectangle's position.
-        """
-        self.__y = value
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
