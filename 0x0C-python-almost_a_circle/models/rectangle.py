d021ef92e7e6ccd1b054b6d6cdcc0d4dd49ee1b6
Mawunyo on axl sandbox-turtle python <dmashigbi@gmail.com>
2023-06-19 18:04:34 -0700

    Add models/rectangle.py and tests/test_models/test_rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+"""This module contains a rectangle class"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Represents a rectangle.
+    Inherits from the Base class.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new instance of the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position.
+                Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's position.
+                Defaults to 0.
+            id (int, optional): The id of the rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter for the width attribute.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for the width attribute.
+
+        Args:
+            value (int): The width of the rectangle.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter for the height attribute.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for the height attribute.
+
+        Args:
+            value (int): The height of the rectangle.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter for the x-coordinate attribute.
+
+        Returns:
+            int: The x-coordinate of the rectangle's position.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter for the x-coordinate attribute.
+
+        Args:
+            value (int): The x-coordinate of the rectangle's position.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter for the y-coordinate attribute.
+
+        Returns:
+            int: The y-coordinate of the rectangle's position.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter for the y-coordinate attribute.
+
+        Args:
+            value (int): The y-coordinate of the rectangle's position.
+        """
+        self.__y = value
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def test_constructor(self):
+        # Test case: Creating a rectangle with default values
+        r = Rectangle(10, 20)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 20)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+        self.assertIsNone(r.id)
+
+        # Test case: Creating a rectangle with custom values
+        r = Rectangle(5, 5, 2, 3, 123)
+        self.assertEqual(r.width, 5)
+        self.assertEqual(r.height, 5)
+        self.assertEqual(r.x, 2)
+        self.assertEqual(r.y, 3)
+        self.assertEqual(r.id, 123)
+
+    def test_getters_and_setters(self):
+        r = Rectangle(10, 10)
+
+        # Test case: Testing width getter and setter
+        r.width = 20
+        self.assertEqual(r.width, 20)
+
+        # Test case: Testing height getter and setter
+        r.height = 15
+        self.assertEqual(r.height, 15)
+
+        # Test case: Testing x-coordinate getter and setter
+        r.x = 5
+        self.assertEqual(r.x, 5)
+
+        # Test case: Testing y-coordinate getter and setter
+        r.y = 8
+        self.assertEqual(r.y, 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
