350cb32d0eb00b047b425d3148e957b4bb6996b7
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-08 22:13:39 +0100

    Add project task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+result = '';
+if (!isNaN(parseInt(process.argv[2]))) {
+  for (let i = 0; i < process.argv[2]; i++) {
+  result += 'x';
+  }
+} else {
+  console.log('Missing size');
+}
+for (let j = 0; j < process.argv[2]; j++) {
+  console.log(result);
+}
