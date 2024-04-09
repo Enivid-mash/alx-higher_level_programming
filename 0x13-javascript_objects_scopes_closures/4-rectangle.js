bb23ca8879f6503596e4e9f42af51c10f005cecb
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-09 21:13:20 +0100

    Add project task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return 'Rectangle {}';
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let result = '';
+    for (let i = 0; i < this.width; i++) {
+      result += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(result);
+    }
+  }
+
+  double () {
+    this.width += this.width;
+    this.height += this.height;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+// Export the class a module
+module.exports = Rectangle;
