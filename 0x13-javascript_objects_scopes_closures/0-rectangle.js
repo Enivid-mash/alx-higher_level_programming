aca40209a9a23e84d5b16076681b35af19e49efa
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-09 19:30:48 +0100

    add project tasks 0, 1 and 2

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor () {
+  }
+}
+
+// Export the class a module
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// Export the class a module
+module.exports = Rectangle;
