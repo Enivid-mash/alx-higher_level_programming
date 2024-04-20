3cf9c48352c216d61dca68590efda0fbe60492b5
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-20 04:39:15 +0100

    Add task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Return matching states with parameters provided to the script: username,
+password, database, and state to match.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    query = """SELECT cities.id, cities.name, states.name FROM states INNER
+    JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC"""
+
+    db_cursor.execute(query, (argv[4],))
+
+    rows_selected = db_cursor.fetchall()
+
+    print(', '.join([f"{row[0]}" for row in rows_selected]))
+
+    db_cursor.close()
+    db_connect.close()
