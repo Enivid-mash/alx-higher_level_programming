793f3d46464af3fad45115bb2e6b15a89fa18445
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-20 04:17:52 +0100

    Add task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Return matching states with parameters provided to the script: username,
+password, database, and state to match.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name FROM states INNER
+    JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC"
+
+    db_cursor.execute(query)
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
+
+    db_cursor.close()
+    db_connect.close()
