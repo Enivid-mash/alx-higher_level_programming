9d4ec3834838a52ae67249fbf8e0b97795ec005a
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-20 01:38:01 +0100

    amend task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""
+Access to the database and get the states
+from the database.
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
@@ -19,3 +19,5 @@ if __name__ == '__main__':
 
     for row in rows_selected:
         print(row)
+    db_cursor.close()
+    db_connect.close()
