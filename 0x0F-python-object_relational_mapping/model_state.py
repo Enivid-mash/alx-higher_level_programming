54ca22bcc23a35605315564092b2bcdfc1b65a19
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-20 05:07:51 +0100

    Add project task 6 and amend task 5

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class definition 
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    instance of Base and  linked to MySQL table "states"
+    """
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
