7bb27174336f48f0bcf3312f23aa78d904e896d0
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-20 03:44:21 +0100

    Add tast 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Return matching states with parameters provided to the script: username,
+password, database, and state to match.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+
+    # Use a parameterized query to select matching states
+    # Instead of formatting the query string directly, we use placeholders (%s)
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+
+    # Pass the state name as a parameter to execute() method
+    db_cursor.execute(query, (argv[4],))
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        if row[1] == argv[4]:
+            print(row)
+
+    db_cursor.close()
+    db_connect.close()
